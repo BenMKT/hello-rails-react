8797d955c01b867cf6dbf6449b247183a25d4dc4
Benson Kiai <benson.kiai86@gmail.com>
2023-12-01 13:55:12 +0300

    Add React components and Redux actions/reducers

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import App from '../components/App';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Router>
+      <Route path="/" component={App} />
+    </Router>,
+    document.getElementById('root')
+  );
+});
